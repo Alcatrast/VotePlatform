5c98f01f9b058a296c88fdaa1cec91706ee34501
Alcatrast <leonid.efremov.99@gmail.com>
2023-01-19 23:07:47 +0300

    begin fix to net 3.1 117 the fuck errors

--- VotePlatform/Models/DataBaseAPI/OrganizationsDataBaseAPI.cs
+++ VotePlatform/Models/DataBaseAPI/OrganizationsDataBaseAPI.cs
@@ -1,26 +1,29 @@
 ﻿
+using System;
 using System.Collections.Generic;
 
 using VoteM.Models.Organizations;
 using VoteM.Models.SystemServices;
 using VoteM.Models.Users;
 using VoteM.Models.DataBaseAPI.SystemServices;
+using VoteM.Models.Organizations.DB;
+using VoteM.Models.Organizations.Serializable;
 
 namespace VoteM.Models.DataBaseAPI
 {
     public static class OrganizationsDataBaseAPI
     {
-        private static OrganizationDataBase dataBase = new(string.Empty);
+        private static OrganizationDataBase dataBase = new OrganizationDataBase(string.Empty);
 
-        public static void Initialize(string connectionString)=>dataBase=new(connectionString);
+        public static void Initialize(string connectionString)=>dataBase=new OrganizationDataBase(connectionString);
         public static bool Create(string organizationNick, string createrUserId)
         {
             if (dataBase.GetByNick(organizationNick).Count != 0) { return false; }
 
             UsersDataBaseAPI.FindById(createrUserId, out User user);
-            Organization organization = new(GetId(), organizationNick, new OrganizationAttributes(), new Meta(), new OrganizationMembers(createrUserId));
+            Organization organization = new Organization(GetId(), organizationNick, new OrganizationAttributes(), new Meta(), new OrganizationMembers(createrUserId));
             if ((sbyte)user.Role < (sbyte)RoleInPlatform.User) { return false; }
-            if (dataBase.Add(new(organization))==false) { return false; }
+            if (dataBase.Add(new OrganizationInDB(organization))==false) { return false; }
             
             user.Membersip.OwnerInGroups.Add(organization.Id);
             UsersDataBaseAPI.Update(user);
@@ -29,18 +32,18 @@ namespace VoteM.Models.DataBaseAPI
         public static bool Update(Organization organization)
         {
             if ((dataBase.GetById(organization.Id).Count != 0 && dataBase.GetByNick(organization.Nickname).Count != 0) == false) { return false; }
-            return dataBase.Update(new(organization));
+            return dataBase.Update(new OrganizationInDB(organization));
         }
         public static bool FindById(string id, out Organization organization)
         {
-            organization = new(new());
+            organization = new Organization(new SOrganizationV1());
             var res=dataBase.GetById(id);
             if(res.Count == 0) { return false; }
             else { organization = res[0].Construct(); return true; }
         }
         public static bool FindByNick(string nick, out Organization organization)
         { 
-            organization = new(new());
+            organization = new Organization(new SOrganizationV1());
             var res = dataBase.GetByNick(nick);
             if (res.Count == 0) { return false; }
             else { organization = res[0].Construct(); return true; }
