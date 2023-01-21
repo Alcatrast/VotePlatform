b5a3009c8d82b9580044468786daf529dc5704bc
Ефремов Леонид <email>
2023-01-21 23:39:12 +0300

    already better/ votecontroller..

--- VotePlatform/Controllers/VotesController.cs
+++ VotePlatform/Controllers/VotesController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VotePlatform.Models.Votes;
+using VotePlatform.Models.Votes.Serializable;
+using VotePlatform.ViewModels.Votes;
 
 namespace VotePlatform.Controllers
 {
     public class VotesController : Controller
     {
-        public string Index()
+        public ViewResult Vote()
         {
-            return "jffjhj";
+            var dv = new DemoVote(new VoteDemoResponse(new Vote(new SVoteV1() { Meta=new SVoteMetaV1() { Header="да.", Description="я смог это."} })));
+            return View(dv);
         }
     }
 }
--- VotePlatform/Startup.cs
+++ VotePlatform/Startup.cs
@@ -30,10 +30,9 @@ namespace VotePlatform
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Votes}/{action=Vote}/{id?}");
             });
         }
     }
