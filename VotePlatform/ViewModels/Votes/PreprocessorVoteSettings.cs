edb16f7519b375c63205c77e1ebf2025a3f01a86
Ефремов Леонид <email>
2023-01-30 03:13:55 +0300

    creating build

---
+++ VotePlatform/ViewModels/Votes/PreprocessorVoteSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace VotePlatform.ViewModels.Votes
+{
+    public class PreprocessorVoteSettings
+    {
+        public string OwnerOrganization { get; }
+        public int CountAnswers { get; }
+        public PreprocessorVoteSettings(string ownerOrganization, int countAnswers)
+        {
+            OwnerOrganization = ownerOrganization;
+            CountAnswers = countAnswers;
+        }
+    }
+}
