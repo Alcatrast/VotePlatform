1f1b6c06d63ba6672d3544ebcd68f8fee4fcbd83
Alcatrast <leonid.efremov.99@gmail.com>
2023-01-30 10:17:23 +0300

    арсен на месте

--- VotePlatform/CFG.cs
+++ VotePlatform/CFG.cs
@@ -4,5 +4,7 @@
     {
         public static string D { get; } = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\LaL\source\repos\VotePlatform\DataBase\DBVP.mdf;Integrated Security=True";
         public static string N { get; } = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\RIMUCHER\source\repos\VotePlatform\DataBase\DBVP.mdf;Integrated Security=True";
+        public static string S { get; } = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\xarsy\OneDrive\Рабочий стол\VotePlatform (3)\VotePlatform\DataBase\DBVP.mdf;Integrated Security=True";
+
     }
 }
